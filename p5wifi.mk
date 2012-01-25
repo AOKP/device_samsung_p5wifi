7fa965c1126774cbe62acf40be5cca55344534fc
motley <motley.slate@gmail.com>
2012-01-25 08:08:32 -0500

    -Turn off mobile data for p5wifi -Update system.prop remark

--- p5wifi.mk
+++ p5wifi.mk
@@ -33,9 +33,12 @@ $(call inherit-product, device/samsung/p5-common/p5-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/p5wifi/p5wifi-vendor.mk)
 
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/overlay
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := p5wifi
 PRODUCT_DEVICE := p5wifi
 PRODUCT_MODEL := GT-P7310
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
